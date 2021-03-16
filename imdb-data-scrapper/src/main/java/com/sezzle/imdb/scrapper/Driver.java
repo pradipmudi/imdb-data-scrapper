c1426413d55ac5835e6ce8a1484f8f74aeb25e9c
Pradip Mudi <pradipmudi24@gmail.com>
2021-03-17 01:43:36 +0530

    final commit after adding final fixes with UTF-8 character check in summary string

--- imdb-data-scrapper/src/main/java/com/sezzle/imdb/scrapper/Driver.java
+++ imdb-data-scrapper/src/main/java/com/sezzle/imdb/scrapper/Driver.java
@@ -2,7 +2,7 @@ package com.sezzle.imdb.scrapper;
 
 import java.io.IOException;
 
-import com.sezzle.imdb.scrapper.util.MovieUrlUtil;
+import com.sezzle.imdb.scrapper.util.MovieUrlDataUtil;
 
 public class Driver {
 
@@ -10,7 +10,7 @@ public class Driver {
 		// TODO Auto-generated method stub
 		String chart_url = args[0];
 		int items_count = Integer.valueOf(args[1]);
-		String response = MovieUrlUtil.getMovieShowsList(chart_url,items_count);
+		String response = MovieUrlDataUtil.getMovieShowsList(chart_url,items_count);
 		System.out.println(response);
 
 	}
