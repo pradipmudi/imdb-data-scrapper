aea7677e370f8126d0a079e065c0e3612c9d0bd2
Pradip Mudi <pradipmudi24@gmail.com>
2021-03-17 01:14:20 +0530

    final commit

--- imdb-data-scrapper/src/main/java/com/sezzle/imdb/scrapper/Driver.java
+++ imdb-data-scrapper/src/main/java/com/sezzle/imdb/scrapper/Driver.java
@@ -11,7 +11,7 @@ public class Driver {
 		String chart_url = args[0];
 		int items_count = Integer.valueOf(args[1]);
 		String response = MovieUrlUtil.getMovieShowsList(chart_url,items_count);
-		System.out.println();
+		System.out.println(response);
 
 	}
 
