b3ed4222682746a9ad7805151947cf745c226caa
Pradip Mudi <pradipmudi24@gmail.com>
2021-03-17 01:51:07 +0530

    added how to run from command Line image

--- imdb-data-scrapper/src/main/java/com/sezzle/imdb/scrapper/util/MovieUrlDataUtil.java
+++ imdb-data-scrapper/src/main/java/com/sezzle/imdb/scrapper/util/MovieUrlDataUtil.java
@@ -1,6 +1,7 @@
 package com.sezzle.imdb.scrapper.util;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -13,10 +14,12 @@ import org.jsoup.select.Elements;
 
 import com.sezzle.imdb.scrapper.model.MovieDescription;
 
-public class MovieUrlUtil {
+public class MovieUrlDataUtil {
 	
 	// map to store the url of each movie to fetch the movie/show data respectively
 	private static LinkedHashMap<Integer, String> movieUrlsMap = null;
+	private static final Charset UTF_8 = Charset.forName("UTF-8");
+	private static final Charset ISO = Charset.forName("ISO-8859-1");
 
 	public static String getMovieShowsList(String chart_url, int items_count) throws IOException {
 		movieUrlsMap = new LinkedHashMap<Integer, String>();
@@ -39,7 +42,9 @@ public class MovieUrlUtil {
 	}
 	
 	public static String getMoviesData(LinkedHashMap<Integer, String> movieUrlsMap) throws IOException {
+		// declaring list to add description of each movie
 		List<MovieDescription> movieDescriptionList = new ArrayList<MovieDescription>();
+		
 		for(Integer index : movieUrlsMap.keySet()) {
 			String movieUrl = movieUrlsMap.get(index);
 			Document doc = Jsoup.connect(movieUrl).get();
@@ -48,15 +53,18 @@ public class MovieUrlUtil {
 			Elements summaryWrapper = doc.select("div.plot_summary_wrapper > div.plot_summary > div.summary_text");
 			Elements genreWrapper = titleWrapper.select("div.subtext");
 			
+			// getting the genres for the movie
 			StringJoiner genres = new StringJoiner(",");
 			for(Element element : genreWrapper) {
 				String href = element.select("a").get(0).attr("href").toString();
 				String[] genreArray = element.select("a").text().split(" ");
+				// taking "length-4" cause we don't want to add the release date data from the last "a tag href"
 				for(int i=0;i<genreArray.length-4;i++) {
 					genres.add(genreArray[i]);
 				}
 			}
 			
+			// getting the movie name and year from titleWrapper h1 text
 			String[] movieNameAndYear = titleWrapper.select("h1").text().split("\\("); 
 			
 			/*
@@ -67,26 +75,25 @@ public class MovieUrlUtil {
 			 * text()); System.out.println("Genres : "+genres);
 			 * System.out.println("Summary : "+summaryWrapper.text()+"\n\n");
 			 */
-			
+
 			MovieDescription movieDescription = new MovieDescription();
 			movieDescription.setTitle(movieNameAndYear[0]);
 			movieDescription.setGenre(genres.toString());
 			movieDescription.setDuration(titleWrapper.select("div.subtext > time").text());
 			movieDescription.setImdb_rating(Double.valueOf(ratingValue.text()));
 			movieDescription.setMovie_release_year(Integer.valueOf(movieNameAndYear[1].replace(")", "")));
-			movieDescription.setSummary(summaryWrapper.text());
+			movieDescription.setSummary(new String(summaryWrapper.text().toString().getBytes(ISO), UTF_8));
 			
 			movieDescriptionList.add(movieDescription);
 			
 		}
-//		System.out.println(JsonUtil.getInstance().toJson(movieDescriptionList));
+		
+		// clearing the URL map
 		movieUrlsMap.clear();
+		
 		return JsonUtil.getInstance().toJson(movieDescriptionList);
 		
 	}
 	
-	public static void main(String[] args) throws IOException {
-		getMovieShowsList("https://www.imdb.com/india/top-rated-indian-movies/", 5);
-	}
 
 }
